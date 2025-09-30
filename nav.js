00a7019cb44609003f3d41121701317e0e054135
Matt Dolin <albeto4000@gmail.com>
2025-09-30 16:09:40 -0400

    Initial commit/added olympics project

---
+++ nav.js
@@ -0,0 +1,11 @@
+navbar = `<nav class="navbar navbar-dark bg-dark pt-3 pb-2 px-3 justify-content">
+    <a class="navbar-brand text-success" href="https://albeto4000.github.io/">MATTHEW DOLIN</a>
+    <div>
+        <div class="navbar-text text-capitalize">
+            ` + document.title + `
+        </div>
+    </div>
+    <div></div>
+</nav>`;
+
+document.write(navbar);
